375f31a00d080e964779e601ca38b6137866a864
kodcanlisi <developerindiee@gmail.com>
2020-12-26 21:50:31 +0300

    Get data based on selected date

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const api = "https://api.exchangeratesapi.io/";
 
 const currencyOne = document.getElementById("currency_one");
 const currencyTwo = document.getElementById("currency_two");
+const currencyThree = document.getElementById("currency_three");
+const currencyFour = document.getElementById("currency_four");
 const amount = document.getElementById("amount");
 const btnCalc = document.getElementById("btn_calculator");
+const btnHistory = document.getElementById("btn_history");
 const result = document.getElementById("result");
+const resultHistory = document.getElementById("result_history");
+const dateInput = document.getElementById('date_input');
 
 const loadCurrency = async () => {
   try {
@@ -20,6 +25,8 @@ const loadCurrency = async () => {
     }
     currencyOne.innerHTML += options;
     currencyTwo.innerHTML += options;
+    currencyThree.innerHTML += options;
+    currencyFour.innerHTML += options;
   } catch (error) {
     alert("Currencies could not be loaded :( " + error);
   }
@@ -42,5 +49,22 @@ btnCalc.addEventListener("click", async () => {
     }`;
   } catch (error) {
     alert("Something went wrong :( " + error);
+  }
+});
+
+btnHistory.addEventListener('click', async () => {
+  const selectedDay = dateInput.value;
+  const previousDay = new Date((new Date(selectedDay)).valueOf() - 1000*60*60*24);
+  const previousDayFormat = `${previousDay.getFullYear()}-${(previousDay.getMonth()+1)}-${previousDay.getDate()}`
+
+  try {
+    const response = await fetch(`${api}history?start_at=${previousDayFormat}&end_at=${selectedDay}&base=${currencyThree.value}`)
+    const data = await response.json();
+    resultHistory.innerHTML = `
+   1 ${currencyThree.value} = ${data.rates[selectedDay][currencyFour.value]} ${currencyFour.value}
+   at ${selectedDay}
+    `;
+  } catch (error) {
+    alert('Data not found at ' + selectedDay);
   }
 });
